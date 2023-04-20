9a00a9c304d99c7a54530d4959b14d9cb1af78f8
LuccaDe <gianlucas.c88@gmail.com>
2023-04-20 12:47:33 -0300

    Base

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    include_once("templates/header.php")
+?>
+
+<div id="container">
+    <header>
+        <div id="logo">
+            <a href="#" class="navbar-brand">
+                <img src="img/logo2.png">
+            </a>
+        </div>
+
+        <nav>
+        </nav>
+    </header>
+</div>
+<?php
+    include_once("templates/footer.php")
+?>
---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<?php
+    include_once('config/url.php')
+    
+?>
+
+<!Doctype html>
+    <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <title>Maquiagems</title>
+
+            <!--Bootstrap-->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" />
+
+            <!--FontAwesome-->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+
+            <!--Css-->
+            <link rel="stylesheet" href="css/style.css">
+        </head>
+
+        <body>
